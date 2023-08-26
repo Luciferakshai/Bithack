de802d379a1ee504ea0bd749f804ae097c079389
Lucifermorning1122 <142897377+Luciferakshai@users.noreply.github.com>
2023-08-26 14:56:38 +0530

    data

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,40 @@ const mongoose = require('mongoose');
 
 const connectDb = async()=>{
   try{
-      const conn = await mongoose.connect('mongodb://127.0.0.1:27017/users');
-      console.log(`Mongo DB connected successfully `)
+    // Connect to MongoDB
+    mongoose.connect('mongodb://127.0.0.1:27017/your-database-name', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB', err);
+    });
+    
+    // Define a MongoDB schema and model for your data
+    const leaderboardSchema = new mongoose.Schema({
+        name: String,
+        score: Number
+    });
+    
+    const Leaderboard = mongoose.model('Leaderboard', leaderboardSchema);
+    
+    // Example: Insert data into the leaderboard
+    const newPlayer = new Leaderboard({
+        name: 'Tej',
+        score: 950
+    });
+    
+    newPlayer.save()
+    .then(() => {
+        console.log('Player added to leaderboard');
+    })
+    .catch((err) => {
+        console.error('Error adding player to leaderboard', err);
+    });
+    
   }catch(e){
     console.log(e)
   }
