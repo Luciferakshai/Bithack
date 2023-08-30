e7e9cb7689f6a0e12d878ef54d7d2b583460a10b
harithvarakesan <harithvarakesanbs@gmail.com>
2023-08-30 12:16:07 +0530

    h

--- BITHACK/create-event.php
+++ BITHACK/create-event.php
@@ -17,8 +17,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $filename = $_POST["filename"];
     
     $sql = "INSERT INTO `events` (`name`, `event_type`, `start_date`, `end_date`, `start_time`, `end_time`, `venue`, `description`, `eligibility`, `filename`) VALUES ('$eventname', '$eventtype', '$sdate', '$edate', '$stime', '$etime', '$venue', '$description', '$eligibility', '$brochurePath')";
-
+    $sql2 = "CREATE TABLE $eventname (
+        s_no INT(255) AUTO_INCREMENT PRIMARY KEY,
+        name TEXT(255),
+        rollnumber VARCHAR(255),
+        emailid VARCHAR(255),
+        department TEXT(255),
+        mark int(255)
+    )";
     $result = $conn->query($sql);
+    $result2 = $conn->query($sql2);
     header("Location: event.html");
 
     $conn->close();
